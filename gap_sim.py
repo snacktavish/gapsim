b84eb0ddbf54fd1472862af64db1b36cd2250c37
Emily Jane McTavish <ejmctavish@gmail.com>
2014-04-17 13:36:30 -0500

    created file

---
+++ gap_sim.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+import random
+import itertools
+# 4 taxon case
+# 2 states
+# 81 possible patterns
+states = ['-',0, 1]
+
+poss_states = set()
+
+import itertools
+s=[ states, states, states, states ]
+poss=list(itertools.product(*s))
+
+
+\[Lambda] = 0.5;
+\[Mu] = 1;
+ta = .01
+tb = 1
+ti = ta/100
+tc = tb
+td = ta
+
+
+# Tree:
+#  A          C
+#    \       /
+#     \     /
+#      \___/
+#      /   \
+#     B     D
+
+#Expected site pattern probabilities.
+
+#calculate distance A-B
+#A-C
+#A-D
+
+
