1527635a074c34b8edcebe9eb53474eca49a6dbe
Mark T. Holder <mtholder@gmail.com>
2015-07-03 10:06:42 +0200

    sb lengthened a bit

--- do_sims.sh
+++ do_sims.sh
@@ -7,7 +7,7 @@ do
         dirn="rep$i"
         mkdir "${dirn}"
         cd "${dirn}"
-        python ../gap_sim.py --sb=0.02 --lb=0.4 -l 100000 > sim.nex 2>sim-err.txt || exit
+        python ../gap_sim.py --sb=0.025 --lb=0.4 -l 100000 > sim.nex 2>sim-err.txt || exit
         paup -n ../master.nex
         paired_invariants_cull.py --p-inv=0.5 nontransposed.nex > culled.nex || exit
         paup -n ../masterculled.nex
