c8848e2f59854cd76cc35745ac4d375d5a23b7f9
Mark T. Holder <mtholder@gmail.com>
2015-07-06 15:08:48 +0200

    lba stats

--- summarize.sh
+++ summarize.sh
@@ -2,6 +2,8 @@
 i=1
 totincc=0
 totcc=0
+totinclba=0
+totlba=0
 while true
 do
         dirn="rep$i"
@@ -15,6 +17,16 @@ do
         then
                 totcc=$(expr 1 + $totcc)
         fi
+        incdistfromlba=$(grep -E '^[3]   [0-2]+ ' "${dirn}/sim.log" | awk '{print $2}' | head -n1)
+        cdistfromlba=$(grep -E '^[3]   [0-2]+ ' "${dirn}/sim.log" | awk '{print $2}' | tail -n1)
+        if test $incdistfromlba -eq 0
+        then
+                totinclba=$(expr 1 + $totinclba)
+        fi
+        if test $cdistfromlba -eq 0
+        then
+                totlba=$(expr 1 + $totlba)
+        fi
         i=$(expr 1 + $i)
         ndirn="rep$i"
         if ! test -d $ndirn
@@ -25,3 +37,5 @@ done
 echo $(expr $i - 1) " runs"
 echo "$totincc correct from raw analysis"
 echo "$totcc correct from culled analysis"
+echo "$totinclba correct from raw analysis"
+echo "$totlba correct from culled analysis"
